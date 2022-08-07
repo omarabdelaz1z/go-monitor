9e8094f07143d39dfc65c4f802abb6b77906a31e
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-08-07 12:38:00 +0200

    enhancements: introduced goroutines! - CaptureStat: capture statistics every second - CaptureInterrupt: waits for a keyboard interrupt and gracefully shutdown the service. - PrintStat: receive buffered netstat and pretty print it.

--- main.go
+++ main.go
@@ -8,127 +8,96 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fatih/color"
+	"github.com/omarabdelaz1z/go-monitor/util"
 	"github.com/shirou/gopsutil/v3/net"
 )
 
 const (
-	DELAY         time.Duration = 1 * time.Second
-	PREVIOUS_LINE string        = "\033[1A\033[K"
+	DELAY                = 1 * time.Second
+	PREVIOUS_LINE string = "\033[1A\033[K" // hacky way to clear the previous line.
 )
 
 var (
-	total *NetStat = &NetStat{
-		bytesSent:  0,
-		bytesRecv:  0,
-		bytesTotal: 0,
-	}
-	logger *log.Logger = log.New(os.Stdout, fmt.Sprintf("[ %s ] ", time.Now().Format(time.RFC822)), 0)
-
-	RateTextFunc     = color.New(color.FgBlack).Add(color.BgHiWhite).SprintFunc()
-	DownloadTextFunc = color.New(color.FgGreen).SprintfFunc()
-	UploadTextFunc   = color.New(color.FgCyan).SprintfFunc()
-	TotalTextFunc    = color.New(color.FgYellow).SprintfFunc()
+	logger         *log.Logger = log.New(os.Stdout, fmt.Sprintf("[%s] ", time.Now().Format(time.RFC822)), 0)
+	cumulativeStat *NetStat    = NewNetStat(0, 0)
 )
 
-type NetStat struct {
-	bytesSent  uint64
-	bytesRecv  uint64
-	bytesTotal uint64
-}
+func PrintStat(schan <-chan *NetStat, quit <-chan bool) {
+	for {
+		select {
+		case <-quit:
+			return
+		case stat, ok := <-schan:
+			if ok {
+				cumulative := util.ByteCountSI(cumulativeStat.bytesTotal)
 
-func (netStat *NetStat) Incr(new *NetStat) {
-	netStat.bytesRecv += new.bytesRecv
-	netStat.bytesSent += new.bytesSent
-	netStat.bytesTotal += new.bytesTotal
-}
-
-func (current *NetStat) Delta(previous *NetStat) *NetStat {
-	return &NetStat{
-		bytesSent:  current.bytesSent - previous.bytesSent,
-		bytesRecv:  current.bytesRecv - previous.bytesRecv,
-		bytesTotal: current.bytesTotal - previous.bytesTotal,
+				logger.Printf(
+					"%s %s\n",
+					stat,
+					util.CumulativeTextFunc("cumulative: %s", util.RateTextFunc(cumulative)),
+				)
+			}
+		}
 	}
 }
 
-func NewNetStat(sent uint64, recv uint64) *NetStat {
-	return &NetStat{
-		bytesSent:  sent,
-		bytesRecv:  recv,
-		bytesTotal: sent + recv,
+func CaptureStat(buffer chan<- *NetStat, quit <-chan bool) {
+	stats, err := net.IOCounters(false)
+
+	if err != nil {
+		logger.Panicf("failed to start capturing stats: %s", err)
+	}
+
+	lastStat := NewNetStat(stats[0].BytesSent, stats[0].BytesRecv)
+
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			stats, err := net.IOCounters(false)
+
+			if err != nil {
+				logger.Panicf("failed to get net stats: %s", err)
+			}
+
+			netstat := NewNetStat(stats[0].BytesSent, stats[0].BytesRecv)
+
+			delta := netstat.Delta(lastStat)
+			cumulativeStat.Incr(delta)
+
+			buffer <- delta
+
+			// replace previous stat with current stat.
+			lastStat = netstat
+
+			time.Sleep(DELAY)
+			fmt.Print(PREVIOUS_LINE)
+		}
 	}
 }
 
-func PrettyStat(stat *NetStat) {
-	prettySent := ByteRepr(stat.bytesSent)
-	prettyRecv := ByteRepr(stat.bytesRecv)
-	prettyTotal := ByteRepr(stat.bytesTotal)
-
-	logger.Printf(
-		"%s %s %s\n",
-		DownloadTextFunc("download: %s", RateTextFunc(prettyRecv)),
-		UploadTextFunc("upload: %s", RateTextFunc(prettySent)),
-		TotalTextFunc("total: %s", RateTextFunc(prettyTotal)),
-	)
-}
-
-// This function is written & modified from Go blueprints.
-// A website that provide code for com­mon tasks is a collection of handy code examples.
-// https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
-
-func ByteRepr(bytes uint64) string {
-	const unit = 1000
-
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-
-	div, exp := uint64(unit), 0
-
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-
-	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "kM"[exp])
+func CaptureInterrupt(sig <-chan os.Signal, quit chan<- bool) {
+	<-sig
+	quit <- true
 }
 
 func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 
-	go func() {
-		<-sig
+	quit := make(chan bool, 1)
+	buffer := make(chan *NetStat)
 
-		fmt.Print(PREVIOUS_LINE)
+	go CaptureInterrupt(sig, quit)
+	go CaptureStat(buffer, quit)
+	go PrintStat(buffer, quit)
 
-		fmt.Print("Capture:\n")
-		PrettyStat(total)
-		os.Exit(0)
-	}()
+	<-quit
+	close(quit)
+	close(buffer)
+	close(sig)
 
-	if stats, err := net.IOCounters(false); err != nil {
-		panic(err)
-	} else {
-		lastNetStat := NewNetStat(stats[0].BytesSent, stats[0].BytesRecv)
-
-		for {
-			stats, err := net.IOCounters(false)
-
-			if err != nil {
-				panic(err)
-			}
-
-			currentNetStat := NewNetStat(stats[0].BytesSent, stats[0].BytesRecv)
-			delta := currentNetStat.Delta(lastNetStat)
-
-			fmt.Print(PREVIOUS_LINE)
-			PrettyStat(delta)
-
-			total.Incr(delta)
-			lastNetStat = currentNetStat
-
-			time.Sleep(DELAY)
-		}
-	}
+	fmt.Print("Captured: \n")
+	logger.Print(cumulativeStat)
 }
