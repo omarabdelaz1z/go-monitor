7c46efc5ce3b2e6177a1b03639d6fafe0f94317b
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-08-13 12:48:53 +0200

    monitoring logic

---
+++ monitoor/monitor.go
@@ -0,0 +1,27 @@
+package monitoor
+
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/v3/net"
+)
+
+const (
+	PER_INTERFACE bool = true  // capture all interfaces.
+	ALL_INTERFACE bool = false // "all"
+)
+
+// An overall network statistics at the current time.
+func Brief() (*NetStat, error) {
+	stats, err := net.IOCounters(ALL_INTERFACE)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to capture network stat: %v", err)
+	}
+
+	return &NetStat{
+		BytesSent:  stats[0].BytesSent,
+		BytesRecv:  stats[0].BytesRecv,
+		BytesTotal: stats[0].BytesSent + stats[0].BytesRecv,
+	}, nil
+}
