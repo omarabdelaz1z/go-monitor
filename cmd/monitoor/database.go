95717134546370e4eee9768fd0c9b32b04bd1153
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-09-28 14:51:20 +0200

    init new database with config

--- cmd/monitoor/database.go
+++ cmd/monitoor/database.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func NewDatabase(cfg *Config) (*sql.DB, error) {
+	db, err := sql.Open(cfg.db.driver, cfg.db.dsn)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %s", err)
+	}
+
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetConnMaxIdleTime(time.Duration(cfg.db.maxIdleTime))
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %s", err)
+	}
+
+	return db, nil
+}
