7cbbfb5f18d04ea8fc66f82e4b3c7a17c0af3769
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-09-28 15:05:40 +0200

    provided a prototype for monitoor service

--- cmd/monitoor/service.go
+++ cmd/monitoor/service.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/omarabdelaz1z/go-monitor/internal/model"
+	m "github.com/omarabdelaz1z/go-monitor/pkg/monitoor"
+)
+
+type Service struct {
+	// logger *logger.Logger
+	config *Config
+
+	snapshots *model.SnapshotModel
+
+	monitorTicker, captureTicker *time.Ticker
+	cumulativeStat, periodicStat *m.NetStat
+
+	mu sync.RWMutex
+}
+
+func (s *Service) Run() error {
+	return nil
+}
+
+func (s *Service) Monitor(ctx context.Context, buffer chan<- *m.NetStat) error {
+	return nil
+}
+
+func (s *Service) Display(ctx context.Context, buffer <-chan *m.NetStat) error {
+	return nil
+}
+
+func (s *Service) Capture(ctx context.Context, buffer <-chan *m.NetStat) error {
+	return nil
+}
